30a73e12f225dc0ec1d97a16478914ff755c2612
anangyoga <aharwinto@gmail.com>
2024-10-15 14:25:01 +0700

    feat: define colors & typography

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,17 @@
-<template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
-</template>
-
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
+
+<template>
+  <q-page class="bg-primary text-white">
+    <p class="text-h1 heading-1">Lorem ipsum</p>
+    <p class="text-h2 heading-2">Dolor Sit Amet</p>
+    <p class="text-body1 body-large">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quos id
+      tempore ipsum quo repellat quis praesentium iste vitae, delectus numquam
+      eligendi, dolor et, quaerat dolorem veniam labore. Eveniet, quam.
+    </p>
+  </q-page>
+</template>
