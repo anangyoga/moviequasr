285ea70340cb26192f3f99eb15a2926f0445bb8a
anangyoga <aharwinto@gmail.com>
2024-10-16 13:10:29 +0700

    feat: sigin page

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -5,13 +5,45 @@ defineOptions({
 </script>
 
 <template>
-  <q-page class="bg-primary text-white">
-    <p class="text-h1 heading-1">Sign In</p>
-    <p class="text-h2 heading-2">Please sign in</p>
-    <p class="text-body1 body-large">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quos id
-      tempore ipsum quo repellat quis praesentium iste vitae, delectus numquam
-      eligendi, dolor et, quaerat dolorem veniam labore. Eveniet, quam.
-    </p>
+  <q-page class="bg-primary text-white flex flex-center">
+    <div class="wrapper column">
+      <p class="text-h1 heading-1 q-mb-xl">Sign In</p>
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          standout="bg-secondary text-white"
+          v-model="text"
+          label="Email"
+          color="secondary"
+          class="full-width col-8 text-white bg-secondary"
+        />
+        <q-input
+          standout="bg-secondary text-white"
+          v-model="text"
+          label="Password"
+          type="password"
+          color="secondary"
+          class="full-width col-8 text-white bg-secondary"
+        />
+
+        <q-checkbox v-model="right" label="Remember me" color="secondary" />
+        <q-btn
+          to="/"
+          label="Login"
+          type="submit"
+          color="positive"
+          class="full-width"
+        />
+      </q-form>
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.wrapper {
+  width: 350px;
+  padding: 0 10px;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+</style>
