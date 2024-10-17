7027cd267a5a3c50636748b11ee899f14f93b75c
anangyoga <aharwinto@gmail.com>
2024-10-17 08:53:36 +0700

    feat: fetch data

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,40 @@
 <script setup>
+import MovieCard from "src/components/MovieCard.vue";
+import { ref, onMounted } from "vue";
+
 defineOptions({
   name: "IndexPage",
+});
+
+const movies = ref([]);
+
+const getData = async () => {
+  const res = await fetch(process.env.API);
+  const data = await res.json();
+
+  movies.value = data.data;
+};
+
+onMounted(() => {
+  getData();
 });
 </script>
 
 <template>
-  <q-page class="bg-primary text-white">
-    <p class="text-h1 heading-1">Movie List</p>
-    <p class="text-h2 heading-2">Check all the movie lists</p>
-    <p class="text-body1 body-large">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quos id
-      tempore ipsum quo repellat quis praesentium iste vitae, delectus numquam
-      eligendi, dolor et, quaerat dolorem veniam labore. Eveniet, quam.
-    </p>
+  <q-page class="bg-primary text-white q-pa-xl">
+    <div class="row justify-between items-center q-mb-xl">
+      <div class="text-h1 heading-1">
+        My movies <q-btn outline round icon="add" to="/create" />
+      </div>
+      <div>
+        <q-btn flat size="13px">
+          <div>Logout</div>
+          <q-icon right size="3em" name="logout" />
+        </q-btn>
+      </div>
+    </div>
+    <div class="row q-gutter-xl">
+      <MovieCard :movies="movies" />
+    </div>
   </q-page>
 </template>
